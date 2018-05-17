806e2264d3c936b65ea787c47415faa8d2055e4b
Xu Liangzhan <xu_liangzhan@163.com>
2018-05-18 00:07:46 +0800

    update 1.5.33

--- dist/xe-utils.js
+++ dist/xe-utils.js
@@ -1,5 +1,5 @@
 /**
- * xe-utils.js v1.5.32
+ * xe-utils.js v1.5.33
  * (c) 2017-2018 Xu Liangzhan
  * ISC License.
  * @preserve
@@ -13,7 +13,7 @@
 
   function XEUtils () { }
 
-  XEUtils.version = '1.5.32'
+  XEUtils.version = '1.5.33'
 
   /**
     * 数组去重
