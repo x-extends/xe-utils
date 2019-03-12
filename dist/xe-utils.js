816cad2739d27768c08273f51561f89b372d8996
xuliangzhan <xu_liangzhan@163.com>
2019-03-12 23:08:43 +0800

    update 1.8.10

--- dist/xe-utils.js
+++ dist/xe-utils.js
@@ -15,16 +15,13 @@
 
   var formatString = 'yyyy-MM-dd HH:mm:ss'
   var setupDefaults = {
-    cookies: null,
     treeOptions: {
-      strict: false,
       parentKey: 'parentId',
       key: 'id',
       children: 'children'
     },
     formatDate: formatString + '.SSS',
     formatString: formatString,
-    formatStringMatchs: null,
     dateDiffRules: [
       ['yyyy', 31536000000],
       ['MM', 2592000000],
