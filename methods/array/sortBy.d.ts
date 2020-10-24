c4550f01d43eb676727751cbd422327922f536c8
xuliangzhan <xu_liangzhan@163.com>
2020-10-24 12:46:21 +0800

    update

--- methods/array/sortBy.d.ts
+++ methods/array/sortBy.d.ts
@@ -1,5 +1,13 @@
+import { XEUtilsMethods } from '../xe-utils'
+
 import orderBy from './orderBy'
 
 export const sortBy: typeof orderBy
+
+declare module '../xe-utils' {
+  interface XEUtilsMethods {
+    sortBy: typeof sortBy;
+  }
+}
 
 export default sortBy
