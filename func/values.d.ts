4e0f04c9e56372b2e578cac74cfb87b53ace5bc8
xuliangzhan <xu_liangzhan@163.com>
2021-11-14 14:41:44 +0800

    update ts

--- func/values.d.ts
+++ func/values.d.ts
@@ -2,7 +2,7 @@
  * 获取对象所有值
  * @param obj 对象
  */
-export declare function values<T>(list: T[]): T[];
+export declare function values<T>(obj: { [s: string]: T } | T[]): T[];
 export declare function values(obj: any): any[];
 
 declare module './ctor' {
