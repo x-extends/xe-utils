5746663c5f83280de1de3f8c22a96a502101bdaa
xuliangzhan <xu_liangzhan@163.com>
2023-06-06 22:10:52 +0800

    fix

--- func/uniq.d.ts
+++ func/uniq.d.ts
@@ -4,8 +4,8 @@
  * @param iterate 回调/对象属性
  * @param context 上下文
  */
-export declare function uniq<T, C = any>(list: T[], iterate: string | number | ((this: C, item: T, index: number, obj: T[]) => string | number), context?: C): T[];
-export declare function uniq<C = any>(list: any, iterate: string | number | ((this: C, item: any, index: number, obj: any) => string | number), context?: C): any[];
+export declare function uniq<T, C = any>(list: T[], iterate?: string | number | ((this: C, item: T, index: number, obj: T[]) => string | number), context?: C): T[];
+export declare function uniq<C = any>(list: any, iterate?: string | number | ((this: C, item: any, index: number, obj: any) => string | number), context?: C): any[];
 
 declare module './ctor' {
   interface XEUtilsMethods {
