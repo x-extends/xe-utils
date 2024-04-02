718556fc2b661e7d3b835d9efb6c00500ce1c9bf
xuliangzhan <xu_liangzhan@163.com>
2024-04-02 17:32:39 +0800

    fix cookie类型有问题 #63

--- func/cookie.d.ts
+++ func/cookie.d.ts
@@ -28,20 +28,20 @@ export interface CookieOptions {
 /**
  * Cookie 操作函数
  */
-export declare function CookieFunc(): cookie;
+export declare function CookieFunc(): XECookie;
 
 /**
  * Cookie 操作函数
  * @param name 键/数组/对象
  */
-export declare function CookieFunc(name: string): cookie;
+export declare function CookieFunc(name: string): XECookie;
 
 /**
  * Cookie 操作函数
  * @param name 键/数组/对象
  * @param value 值
  */
-export declare function CookieFunc(name: string, value: any): cookie;
+export declare function CookieFunc(name: string, value: any): XECookie;
 
 /**
  * Cookie 操作函数
@@ -49,9 +49,9 @@ export declare function CookieFunc(name: string, value: any): cookie;
  * @param value 值
  * @param options 可选参数
  */
-export declare function CookieFunc(name: string, value: any, options: CookieOptions): cookie;
+export declare function CookieFunc(name: string, value: any, options: CookieOptions): XECookie;
 
-export type cookie = typeof CookieFunc & {
+export type XECookie = typeof CookieFunc & {
   /**
    * 根据 name 判断 Cookie 是否存在
    * @param name 键
@@ -64,7 +64,7 @@ export type cookie = typeof CookieFunc & {
    * @param value 值
    * @param options 可选参数
    */
-  set(name: string, value: any, options?: CookieOptions): cookie;
+  set(name: string, value: any, options?: CookieOptions): XECookie;
 
   /**
    * 根据 name 获取 Cookie
@@ -90,9 +90,11 @@ export type cookie = typeof CookieFunc & {
   getJSON(): any;
 }
 
+export const cookie: XECookie
+
 declare module './ctor' {
   interface XEUtilsMethods {
-    cookie: cookie;
+    cookie: XECookie;
   }
 }
 
