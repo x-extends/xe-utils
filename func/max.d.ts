efd1b696076ab8a398d19a22d4ed348f9dda3f06
xuliangzhan <xlz26296x@163.com>
2024-06-13 11:49:11 +0800

    releases 3.5.27

--- func/max.d.ts
+++ func/max.d.ts
@@ -1,10 +1,9 @@
-
 /**
  * 获取最大值
  * @param list 数组
  * @param iterate 回调/属性
  */
-export declare function max<T, U>(list: T[], iterate: string | number | ((item: T, index: number, list: T[]) => U)): T | U;
+export declare function max<T>(list: T[], iterate: string | number | ((item: T, index: number, list: T[]) => number | string)): T | null | undefined;
 
 declare module './ctor' {
   interface XEUtilsMethods {
--- func/min.d.ts
+++ func/min.d.ts
@@ -3,7 +3,7 @@
  * @param array 数组
  * @param iterate 回调/属性
  */
-export declare function min<T, U>(list: T[], iterate?: string | number | ((item: T, index: number, list: T[]) => U)): T | U;
+export declare function min<T>(list: T[], iterate?: string | number | ((item: T, index: number, list: T[]) => number | string)): T | null | undefined;
 
 declare module './ctor' {
   interface XEUtilsMethods {
