38cb1b9e7f1c1d25dfea914b4488afc40fb82c8b
xuliangzhan <xu_liangzhan@163.com>
2025-03-06 16:35:34 +0800

    releases 3.7.3

--- func/uniqueId.js
+++ func/uniqueId.js
@@ -1,12 +1,14 @@
+var eqNull = require('./eqNull')
+
 /**
   * 获取一个全局唯一标识
   *
   * @param {String} prefix 前缀
   * @return {Number}
   */
-var __uniqueId = 0
+let __uniqueId = 1
 function uniqueId (prefix) {
-  return [prefix, ++__uniqueId].join('')
+  return '' + (eqNull(prefix) ? '' : prefix) + __uniqueId++
 }
 
 module.exports = uniqueId
