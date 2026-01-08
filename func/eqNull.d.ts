d314b78dd914c12de3f33a5ad36e462a7a6e624e
xuliangzhan <xu_liangzhan@163.com>
2026-01-08 21:09:12 +0800

    releases 3.8.4

--- func/eqNull.d.ts
+++ func/eqNull.d.ts
@@ -2,7 +2,7 @@
  * 判断是否 undefined 和 null
  * @param obj 对象
  */
-export declare function eqNull(obj: any): boolean;
+export declare function eqNull(obj: any): obj is null | undefined;
 
 declare module './ctor' {
   interface XEUtilsMethods {
