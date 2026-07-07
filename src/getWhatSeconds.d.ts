68a3f25810125253db310d552df0f30a4d6a9501
xuliangzhan <xu_liangzhan@163.com>
2026-07-07 09:52:01 +0800

    update ts

--- src/getWhatSeconds.d.ts
+++ src/getWhatSeconds.d.ts
@@ -9,7 +9,7 @@ export declare function getWhatSeconds(date: string | Date | number | null | und
  * 返回前几秒或后几秒的日期
  * @param date 字符串/日期/时间戳
  * @param offset 秒偏移量(默认0)、前几秒、后几秒
- * @param mode 指定毫秒(null默认当前毫秒)、0毫秒(first)、59毫秒(last)
+ * @param mode 指定毫秒(null默认当前毫秒)、0毫秒(first)、999毫秒(last)
  */
 export declare function getWhatSeconds(date: string | Date | number | null | undefined, offset: number, mode: 'first' | 'last'): Date;
 
